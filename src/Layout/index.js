83191e4b0b58cf7dd0e00022612f5a396ad72e69
Quadrick <quadtreeder@gmail.com>
2021-08-04 12:18:51 -0700

    First

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { Route, Switch } from "react-router-dom";
+import Home from "../home/Home";
+import Decks from "../decks/Decks";
 
 function Layout() {
   return (
-    <>
+    <React.Fragment>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/decks">
+            <Decks />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
-    </>
+    </React.Fragment>
   );
 }
 
